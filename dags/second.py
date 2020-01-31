d2404d1f58a45936117456d851d718bcdcee6d82
Seth Itschner <sitschner@schubergphilis.com>
2020-01-31 13:24:37 +0100

    python operation in second dag

--- dags/second.py
+++ dags/second.py
@@ -6,51 +6,57 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'Airflow',
     'depends_on_past': False,
-    'start_date': datetime(2020, 1, 30),
+    'start_date': datetime(2020, 1, 25),
     'email': ['seth@ragnarok.net'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
     # 'schedule_interval': "15 08 * * *",
-    'schedule_interval': timedelta(minutes=150)
+    # 'schedule_interval': timedelta(minutes=150)
+    'schedule_interval': "@daily"
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
 }
 
+def _print_exec_date(**context):
+    print(context["execution_date"])
+
+
 dag = DAG('new-time', default_args=default_args, schedule_interval=timedelta(days=1))
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
-t1 = BashOperator(
-    task_id='print_date',
-    bash_command='date',
-    dag=dag)
+t1 = PythonOperator(
+    task_id='print_the_exec_date',
+    provide_context=True,
+    python_callable=_print_exec_date,
+    dag=dag,
+
+
+)
 
 t2 = BashOperator(
-    task_id='sleep',
-    bash_command='sleep 5',
+    task_id='wait_1',
+    bash_command='sleep 1',
     retries=3,
     dag=dag)
 
 t3 = BashOperator(
-    task_id='sleep-3',
+    task_id='wait_5',
     bash_command='sleep 5',
     retries=3,
     dag=dag)
 
 t4 = BashOperator(
-    task_id='sleep-4',
-    bash_command='sleep 5',
+    task_id='wait_10',
+    bash_command='sleep 10',
     retries=3,
     dag=dag)
 
-
-t5 = BashOperator(
-    task_id='sleep-5',
-    bash_command='sleep 5',
-    retries=3,
+t5 = Dummyperator(
+    task_id='Done',
     dag=dag)
 
 
