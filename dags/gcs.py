4aad56d0ad37e9384465d7f6804d73afcf5c56c1
Seth Itschner <sitschner@schubergphilis.com>
2020-02-14 15:55:26 +0100

    initial attempt at a rocket launch

--- dags/gcs.py
+++ dags/gcs.py
@@ -48,6 +48,6 @@ fetch_conversion = HttpToGcsOperator(
     task_id="fetch_converstion",
     gcs_bucket=GCS_BUCKET,
     gcs_path="/converstion",
-    endpoint=f"https://api.exchangeratesapi.io/history?start_at={default_args['start_date']}&end_at={default_args['start_date']}&symbols=EUR&base=GBP",
+    endpoint=f"https://api.exchangeratesapi.io/history?start_at=2019-01-01&end_at=2020-01-01&symbols=EUR&base=GBP",
     dag=dag
 )
