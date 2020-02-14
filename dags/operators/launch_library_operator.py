ff7e04a236cc9b0a35c5bce79bfcda449a4fecc7
Seth Itschner <sitschner@schubergphilis.com>
2020-02-14 11:15:04 +0100

    initial attempt at a rocket launch

--- dags/operators/launch_library_operator.py
+++ dags/operators/launch_library_operator.py
@@ -4,22 +4,37 @@ import posixpath
 import pathlib
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.gcs_hook import GoogleCloudStorageHook
 
 class LaunchLibraryOperator(BaseOperator):
     ui_color = '#555'
     ui_fgcolor = '#fff'
-    template_fields = ('_myvar1',)
+    template_fields = ('ds', 'tomorrow_ds')
 
     @apply_defaults
-    def __init__(self, *args, **kwargs):
+    def __init__(self, task_id, conn_id, endpoints, result_path, result_filename):
         super().__init__(*args, **kwargs)
+        self.task_id = task_id
+        self.conn_id = conn_id
+        self.endpoints = endpoints
+        self.result_path = result_path
+        self.result_filename = result_filename
 
-    def execute(self, ds, tomrrow_ds, **kwargs):
+    def execute(self, ds, tomrrow_ds, result_filename):
         query = f"https://launchlibrary.net/1.4/launch?startdate={ds}&enddate={tomorrow_ds}"
         result_path = f"/tmp/rocket_launches/ds={ds}"
         pathlib.Path(result_path).mkdir(parents=True, exist_ok=True)
         response = requests.get(query)
         print(f"responsewas {response}")
-        with open(posixpath.join(result_path, result_filename), "w") as f:
-            print(f"Writingto file {f.result_filename}")
-            f.write(response.text)
+        upload_to_gcs(self.result_path, 'europe-west1-training-airfl-fdb83332-bucket', self.result_filename)
+
+    def upload_to_gcs(local_path, bucket, destination):
+        gcs = GoogleCloudStorageHook()
+        files_and_dirs = os.listdir(local_path)
+        files_to_upload = [file for file in files_and_dirs if isfile(join(local_path, file))]
+
+        for file in files_to_upload:
+            object = join(destination, file)
+            gcs.upload(bucket, object, join(local_path, file), mime_type='application/octet-stream')
+            # Then delete the file locally
+            os.remove(join(local_path, file))
